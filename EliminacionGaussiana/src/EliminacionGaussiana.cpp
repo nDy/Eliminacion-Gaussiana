9f934533ae36a5c44188d8ab8bd5e5f72fc74abb
ndy <andresrangel.1991@gmail.com>
2012-09-10 14:59:05 +0000

    Alpha 1.0
    Signed-off-by: ndy <andresrangel.1991@gmail.com>

---
+++ EliminacionGaussiana/src/EliminacionGaussiana.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : EliminacionGaussiana.cpp
+// Author      : nDy
+// Version     : 1.0
+//============================================================================
+
+#include <iostream>
+#include "../lib/mmio.h"
+using namespace std;
+
+int main() {
+
+	return 0;
+}
